36d5f5c535ebaf4e4469333f5498999d41f1f18d
zhongwenyu <zhongwenyu@beidukeji.com>
2020-08-27 11:19:28 +0800

    zipkin

--- src/ybrenLib/zipkinphp/LogReport.php
+++ src/ybrenLib/zipkinphp/LogReport.php
@@ -2,12 +2,17 @@
 namespace ybrenLib\zipkinphp;
 
 use ybrenLib\logger\Logger;
+use ybrenLib\logger\utils\StringUtil;
 use Zipkin\Recording\Span;
 use Zipkin\Reporter;
 
 final class LogReport implements Reporter
 {
     private $logger;
+
+    private $logFormat = "%s{<n>}%s{<n>}";
+    private $bulkIndexName = "index";
+    private $indexTypeName = "span";
 
     /**
      * @param Logger $logger
@@ -25,10 +30,23 @@ final class LogReport implements Reporter
         if(empty($spans)){
             return;
         }
-        $spansArr = [];
+     //   $spansArr = [];
+        $str = "";
+        $len = count($spans);
+        $cursor = 1;
         foreach ($spans as $span) {
-            $spansArr[] = $span->toArray();
+            $spanArray = $span->toArray();
+            $spanContent = json_encode($spanArray);
+            $indexContent = json_encode([
+                $this->bulkIndexName => [
+                    "_index" => "zipkin:span-".date("Y-m-d"),
+                    "_type" => $this->indexTypeName,
+                    "_id" => $spanArray['id'] . StringUtil::getRandomStr("zipkin"),
+                ],
+            ]);
+            $str .= sprintf($this->logFormat , $indexContent , $spanContent) . ($cursor == $len ? "" : "\n");
+            $cursor++;
         }
-        $this->logger->info(json_encode($spansArr));
+        $this->logger->info($str);
     }
 }
